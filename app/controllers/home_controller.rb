8f48497c4a2a907a8eef42cdc62d708d1237f16f
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-10 01:15:37 -0400

    started looping through the response and matched it against an array of coins in my wallet to filter through array

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,8 @@ class HomeController < ApplicationController
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @coins = JSON.parse(@response)
+
+    @my_coins = ["BTC", "XRP", "ADA", "XLM", "STEEM"]
   end
 
   def about
