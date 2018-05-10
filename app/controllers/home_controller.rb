5000c919359618d351327a9e14170baaadf4c63c
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-05-10 00:26:07 -0400

    added some nav and simple routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def about
+    @hello = 'some random text '
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  get 'home/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  get 'home/about'
 end
